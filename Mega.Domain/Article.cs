8d0ad49b7bd1991592f5d783d7b700a446c54f5a
vladmartykhin <vladmartykhin@gmail.com>
2018-09-23 13:25:02 +0300

    Так как EF Core не поддерживает связи many-to-many пришлось создать дополнительно ArticleTag

---
+++ Mega.Domain/Article.cs
@@ -0,0 +1,25 @@
+﻿namespace Mega.Domain
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Linq;
+
+    public class Article
+    {
+        public int ArticleId { get; set; }
+
+        public DateTime DateCreate { get; set; }
+
+        public string Text { get; set; }
+
+        public string Head { get; set; }
+
+        public string Url { get; set; }
+
+        public virtual ICollection<ArticleTag> ArticleTags { get; } = new List<ArticleTag>();
+
+        [NotMapped]
+        public IEnumerable<Tag> Tags => this.ArticleTags.Select(e => e.Tag);
+    }
+}
---
+++ Mega.Domain/Tag.cs
@@ -0,0 +1,20 @@
+﻿namespace Mega.Domain
+{
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Linq;
+
+    public class Tag
+    {
+        public int TagId { get; set; }
+
+        public string Uri { get; set; }
+
+        public string Name { get; set; }
+
+        public virtual ICollection<ArticleTag> ArticleTags { get; } = new List<ArticleTag>();
+
+        [NotMapped]
+        public IEnumerable<Article> Articles => this.ArticleTags.Select(e => e.Article);
+    }
+}
