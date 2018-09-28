20e86382e03ae856d3dcba549eac7a72767ffb5f
vladmartykhin <vladmartykhin@gmail.com>
2018-09-28 23:32:29 +0300

    HasForeignKey

--- Mega.Data/DataContext.cs
+++ Mega.Data/DataContext.cs
@@ -19,7 +19,7 @@
         {
             modelBuilder.Entity<ArticleTag>().HasKey(pc => new { pc.ArticleId, pc.TagId });
             modelBuilder.Entity<ArticleTag>().HasOne(pc => pc.Article).WithMany(p => p.ArticleTags).HasForeignKey(pc => pc.ArticleId);
-            modelBuilder.Entity<ArticleTag>().HasOne(pc => pc.Tag).WithMany(c => c.ArticleTags);
+            modelBuilder.Entity<ArticleTag>().HasOne(pc => pc.Tag).WithMany(c => c.ArticleTags).HasForeignKey(pc => pc.TagId);
         }
     }
 }
