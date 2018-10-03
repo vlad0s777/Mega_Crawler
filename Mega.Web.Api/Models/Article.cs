93831037e428e727d7e3c30dfbfcc377aa8e5cc1
vladmartykhin <vladmartykhin@gmail.com>
2018-10-03 09:23:05 +0300

    xml документирование моделей

--- Mega.Web.Api/Models/Article.cs
+++ Mega.Web.Api/Models/Article.cs
@@ -4,20 +4,44 @@
     using System.Collections.Generic;
     using System.Linq;
 
+    /// <summary>
+    /// Модель статьи
+    /// </summary>
     public class Article
     {
+        /// <value>
+        /// ID статьи
+        /// </value>
         public int ArticleId { get; set; }
 
+        /// <value>
+        /// Дата создания статьи
+        /// </value>
         public DateTime DateCreate { get; set; }
 
+        /// <value>
+        /// Текст статьи
+        /// </value>
         public string Text { get; set; }
 
+        /// <value>
+        /// Заголовок статьи
+        /// </value>
         public string Head { get; set; }
 
+        /// <value>
+        /// Идентификатор статьи с сайта
+        /// </value>
         public int OuterArticleId { get; set; }
 
+        /// <value>
+        /// Теги статьи
+        /// </value>
         public IEnumerable<string> Tags { get; set; }
 
+        /// <value>
+        /// Идентификатор статьи с сайта
+        /// </value>
         public int CountTags => this.Tags.Count();
     }
 }
--- Mega.Web.Api/Models/Tag.cs
+++ Mega.Web.Api/Models/Tag.cs
@@ -3,16 +3,34 @@
     using System.Collections.Generic;
     using System.Linq;
 
+    /// <summary>
+    /// Модель тега
+    /// </summary>
     public class Tag
     {
+        /// <value>
+        /// ID тега
+        /// </value>
         public int TagId { get; set; }
 
+        /// <value>
+        /// строковый ключ статьи с сайта
+        /// </value>
         public string TagKey { get; set; }
 
+        /// <value>
+        /// имя тега
+        /// </value>
         public string Name { get; set; }
 
+        /// <value>
+        /// статьи с данным тегом
+        /// </value>
         public IEnumerable<string> Articles { get; set; }
 
+        /// <value>
+        /// количество статей с данным тегом
+        /// </value>
         public int CountArticles => this.Articles.Count();
     }
 }
