6ca2b0c659d313f3fcdd66f23f805e60e2507dc5
vladmartykhin <vladmartykhin@gmail.com>
2018-11-14 11:39:50 +0300

    fix

--- Mega.Services/ZadolbaliClient/ZadolbaliClient.cs
+++ Mega.Services/ZadolbaliClient/ZadolbaliClient.cs
@@ -145,24 +145,25 @@
                     {
                         try
                         {
-                            var articleDoc = await parser.ParseAsync(article.InnerHtml);
-
-                            var head = articleDoc.QuerySelector("h2").TextContent;
-                            var urlArticle = articleDoc.QuerySelector("h2>a").Attributes["href"];
-                            var date = GetDate(articleDoc.QuerySelector("div.meta>div.date-time").InnerHtml);
-                            var content = articleDoc.QuerySelector("div.text").TextContent;
-                            var tagsSelector = articleDoc.QuerySelectorAll("div.meta>div.tags>ul>li>a");
-                            var tags = new List<TagInfo>();
-                            foreach (var selector in tagsSelector)
+                            using (var articleDoc = await parser.ParseAsync(article.InnerHtml))
                             {
-                                var key = selector.Attributes["href"].Value.Split("/").Last();
-                                var text = selector.InnerHtml;
+                                var head = articleDoc.QuerySelector("h2").TextContent;
+                                var urlArticle = articleDoc.QuerySelector("h2>a").Attributes["href"];
+                                var date = GetDate(articleDoc.QuerySelector("div.meta>div.date-time").InnerHtml);
+                                var content = articleDoc.QuerySelector("div.text").TextContent;
+                                var tagsSelector = articleDoc.QuerySelectorAll("div.meta>div.tags>ul>li>a");
+                                var tags = new List<TagInfo>();
+                                foreach (var selector in tagsSelector)
+                                {
+                                    var key = selector.Attributes["href"].Value.Split("/").Last();
+                                    var text = selector.InnerHtml;
 
-                                tags.Add(new TagInfo(key, text));
+                                    tags.Add(new TagInfo(key, text));
+                                }
+
+                                articles.Add(new ArticleInfo(date, tags, content, head, Convert.ToInt32(urlArticle.Value.Split("/").Last())));
+                                this.logger.LogDebug($"Add '{head}' document! Speed: {DownloadStatistic.Speed()}");
                             }
-
-                            articles.Add(new ArticleInfo(date, tags, content, head, Convert.ToInt32(urlArticle.Value.Split("/").Last())));
-                            this.logger.LogDebug($"Add '{head}' document! Speed: {DownloadStatistic.Speed()}");
                         }
                         catch (Exception e)
                         {
